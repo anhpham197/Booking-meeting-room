38544d5d05d50165e2a14bab377b8e25cefe531f
kieuvantuyen01 <tuyenkieuvan@gmail.com>
2021-11-11 17:21:55 +0700

    delete Spatie

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,9 +7,6 @@ use \DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Event extends Model implements HasMedia
 {
@@ -45,37 +42,6 @@ class Event extends Model implements HasMedia
         'updated_at',
         'deleted_at',
     ];
-
-    public function registerMediaConversions(Media $media = null): void
-    {
-        $this->addMediaConversion('thumb')->fit('crop', 50, 50);
-        $this->addMediaConversion('preview')->fit('crop', 120, 120);
-    }
-
-    public function getFileAttribute()
-    {
-        return $this->getMedia('file')->last();
-    }
-
-    // public function getStartDayAttribute($value)
-    // {
-    //     return $value ? Carbon::createFromFormat('Y-m-d H:i:s', $value)->format(config('panel.date_format') . ' ' . config('panel.time_format')) : null;
-    // }
-
-    // public function setStartDayAttribute($value)
-    // {
-    //     $this->attributes['start_day'] = $value ? Carbon::createFromFormat(config('panel.date_format') . ' ' . config('panel.time_format'), $value)->format('Y-m-d H:i:s') : null;
-    // }
-
-    // public function getEndDayAttribute($value)
-    // {
-    //     return $value ? Carbon::createFromFormat('Y-m-d H:i:s', $value)->format(config('panel.date_format') . ' ' . config('panel.time_format')) : null;
-    // }
-
-    // public function setEndDayAttribute($value)
-    // {
-    //     $this->attributes['end_day'] = $value ? Carbon::createFromFormat(config('panel.date_format') . ' ' . config('panel.time_format'), $value)->format('Y-m-d H:i:s') : null;
-    // }
 
     public function rooms()
     {
