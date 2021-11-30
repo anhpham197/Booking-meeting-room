56130f6bd30048571931f3e0c503cba254599fec
kieuvantuyen01 <tuyenkieuvan@gmail.com>
2021-11-30 07:08:33 +0700

    before merge anhpham branch

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Booking</title>
+    <title>Booking</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="/khanh/css/style_for_article.css">
+    {{-- <link href="{{ mix('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" type="text/css" href="/resources/css/style.css" >
+
 </head>
 
 <body>
@@ -19,10 +22,10 @@
             <nav class="navbar navbar-expand-lg navbar-light bg-light" style="height: 60px;" >
                 <div class="flex gap-6">
                     <button type="button" id="sidebarCollapse" class="cursor-pointer rounded-md">
-                        <i class="fas fa-angle-double-left text-2xl font-normal text-gray-400"></i>    
+                        <i class="fas fa-angle-double-left text-2xl font-normal text-gray-400"></i>
                     </button>
                     {{-- <div class="justify-center">Trang chủ</div> --}}
-                    <div class="relative flex w-full flex-wrap items-stretch"> 
+                    <div class="relative flex w-full flex-wrap items-stretch">
                         <span
                           class="z-10 h-full leading-snug font-normal absolutetext-center text-gray-400 absolute bg-transparent rounded items-center justify-center pl-3 py-2">
                           <i class="fas fa-search"></i>
@@ -32,7 +35,6 @@
                     </div>
                 </div>
             </nav>
-<<<<<<< HEAD
             <div class="event-content">
                 <button class="add-event-btn">Thêm sự kiện</button>
                 <p class="event-list-p">Danh sách sự kiện</p>
@@ -50,98 +52,6 @@
                             <p>In</p>
                             <p>Cột</p>
                             <p style="background-color: #e55353; color: white;">Xóa</p>
-=======
-
-            <section class="row">
-                <div class="col-12">
-                    <form autocomplete="on" action="{{url('event/upload')}}" method="POST" enctype="multipart/form-data">
-                        {{-- @method('PUT') --}}
-                        @csrf
-
-                        <div class="card">
-                            <div class="card-header text-center">
-                                <b>Đặt phòng họp</b>
-                            </div>
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <label for="usernameBooking">Họ tên người đặt <span class="text-red-600">*</span></label>
-                                    <input type="text" class="form-control" name="usernameBooking" id="usernameBooking" aria-describedby="usernameBookingHid" required>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="telephoneBooking">Số điện thoại</label>
-                                    <input type="tel" class="form-control" name="telephoneBooking" id="telephoneBooking" aria-describedby="telephoneBookingHid" placeholder="">
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="emailBooking">Email</label>
-                                    <input type="email" class="form-control" name="emailBooking" id="emailBooking" aria-describedby="emailBookingHid" placeholder="">
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col">
-                                            <label for="booking_date_start">Ngày bắt đầu</label>
-                                            <input type="date" class="form-control" name="booking_date_start" id="booking_date_start" >
-                                        </div>
-                                        <div class="col">
-                                            <label for="time_start">Thời gian bắt đầu</label>
-                                            <input type="time" class="form-control" name="time_start" id="time_start" >
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col">
-                                            <label for="booking_date_end">Ngày kết thúc</label>
-                                            <input type="date" class="form-control" name="booking_date_end" id="booking_date_end" >
-                                        </div>
-                                        <div class="col">
-                                            <label for="time_end">Thời gian kết thúc</label>
-                                            <input type="time" class="form-control" name="time_end" id="time_end" >
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="roomId">Mã phòng</label>
-                                    <select name="roomId" id="" class="form-control" id="roomId">
-                                        <option value="405">405</option>
-                                        <option value="406">406</option>
-                                    </select>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="email">Email người tham gia</label>
-                                    <input type="email" class="form-control" id="emails" list="emails" multiple style="width: 100%;">
-                                    <datalist id="emails">
-                                        <option value="19021274@vnu.edu.vn">
-                                        <option value="1234@vnu.edu.vn">
-                                        <option value="23232@vnu.edu.vn">
-                                        <option value="2342312@vnu.edu.vn">
-                                    </datalist>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="textarea">Nội dung cuộc họp</label>
-                                    <textarea id="textarea1" name="description" class="form-control" rows="5"></textarea>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="fileupload">Đính kèm tệp</label>
-                                    <input type="file" name="fileupload" >
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="textarea">Ghi chú</label>
-                                    <textarea id="textarea2" name="note" class="form-control" rows="5"></textarea>
-                                </div>
-                            </div>
-                            <div class="card-footer" style="text-align: center;">
-                                <button type="submit" class="btn btn-primary">Đặt phòng</button>
-                            </div>
->>>>>>> anhpham
                         </div>
                         <div class="right-content">
                             <label for="search">Tìm kiếm:</label>
@@ -236,7 +146,6 @@
             </div>
         </div>
     </div>
-<<<<<<< HEAD
     <footer class="page-footer bg-dark pt-3 pb-5">
 
         <div class="container text-center text-md-left mt-5">
@@ -310,24 +219,6 @@
 </script>
 {{-- </body>
 </html> --}}
-=======
-    {{-- <script type="text/javascript">
-        $('#search').on('keyup',function(){
-            $value = $(this).val();
-            $.ajax({
-                type: 'get',
-                url: '{{ URL::to('search') }}',
-                data: {
-                    'search': $value
-                },
-                success:function(data){
-                    $('#data').html(data);
-                }
-            });
-        })
-        $.ajaxSetup({ headers: { 'csrftoken' : '{{ csrf_token() }}' } });
-    </script> --}}
->>>>>>> anhpham
 @endsection
 </body>
 </html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,16 +6,18 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
     <title>{{ config('app.name', 'KATH') }}</title>
-    
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
-    
-    
+
+
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    {{-- Nếu muốn dùng file ngoài thì như này nhé, ko cần mix, ko cần chạy npm  --}}
+    <link rel="stylesheet" href="/css/style_for_article.css">
 
     <!-- Font -->
     <link href="https://fonts.googleapis.com/css2?family=Jacques+Francois+Shadow&family=Londrina+Shadow&display=swap" rel="stylesheet">
@@ -30,7 +32,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
+
     <!-- JQuery -->
     <script src="{{ asset('js/jquery.min.js') }}"></script>
 
@@ -47,7 +49,7 @@
                 <div class="text-center ml6 sm:pb-4 md:pb-0">
                     <a href="{{ url('/') }}" class="text-4xl text-gray-300 letters font-logo uppercase font-semibold">
                         KATH meeting room
-                    </a> 
+                    </a>
                 </div>
                 <nav class="space-x-4 text-sm sm:text-base text-center">
                     <a class="hover:font-bold hover:no-underline hover:text-gray-300 uppercase text-gray-300" href="{{ route('show_booking') }}">Đặt phòng</a>
